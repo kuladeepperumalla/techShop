978b85c2df16a470a120e758ca3fb72924cc236f
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-09-02 21:26:31 -0400

    added Link from react-router-do

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import React from "react";
-import HomeScreens from "./screens/HomeScreens";
+// import HomeScreens from "./screens/HomeScreens";
+import { Outlet } from "react-router";
 
 function App() {
 
   return (
     <>
       <Header />
-      <main className="'py-3">
+      <main className="py-3">
         <Container>
-          <HomeScreens />
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import HomeScreens from './screens/HomeScreens';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>} >
+      <Route path='/' index={true} element={<HomeScreens />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
