7963f17739af7c426466fa01f7964b459a9ee957
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-09-10 14:38:45 -0400

    added controllers for products

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 dotenv.config();
-import productRoutes from './routes/productRoutes.js'
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -15,5 +15,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
+app.use(notFound);
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server is running at ${port}`));
