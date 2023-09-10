625261bb9b8eefb53bf15048326662a65c5d22a4
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-09-09 22:13:26 -0400

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-dotenv.config()
+dotenv.config();
+import productRoutes from './routes/productRoutes.js'
 import products from './data/products.js';
+const port = process.env.PORT || 5000;
 
-
-const port = process.env.PORT || 5000
 connectDB();
 const app = express();
 
@@ -13,12 +13,7 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get(`/api/products/:id`, (req,res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product)
-})
+
 app.listen(port, () => console.log(`server is running at ${port}`));
