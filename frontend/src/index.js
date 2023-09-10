27c7710021cf5e747bc5a3b013a187eecc832b92
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-09-10 16:19:25 -0400

    frontend: redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store'
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -28,7 +30,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
