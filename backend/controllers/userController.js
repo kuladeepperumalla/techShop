f29ef7c54ea269678a375e01eb9bc6ed622d8faf
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-09-17 22:06:19 -0400

    controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,37 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+
+/**
+@description: Auth User.
+@route: POST /api/users
+@access: public
+ */
+const authUser = asyncHandler(async (req, res) => {
+    res.send('auth user')
+})
+
+/**
+@description: register User.
+@route: POST /api/users
+@access: public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+    res.send('register user')
+})
+/**
+@description: logout User.
+@route: POST /api/users
+@access: private
+ */
+const logoutUser = asyncHandler(async (req, res) => {
+    res.send('auth user')
+})
+/**
+@description: get user profile.
+@route: GET /api/users/profile
+@access: private
+ */
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.send('auth user')
+})
