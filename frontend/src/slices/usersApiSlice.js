0cfcfed228417b3aecd7a1baf97423161f2f4bc9
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-09-20 23:52:58 -0400

    documentation Api 0.01

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -3,6 +3,11 @@ import { apiSlice } from "./apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
+        /**
+        @description Login mutation
+        @method POST
+        @access public
+        */
         login: builder.mutation({
             query: (data) => ({
                 url: `${USERS_URL}/auth`,
@@ -11,6 +16,11 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor: 5
         }),
+        /**
+        @description Register mutation
+        @method POST
+        @access public
+        */
         register: builder.mutation({
             query: (data) => ({
                 url: `${USERS_URL}`,
@@ -18,6 +28,11 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 body: data,
             })
         }),
+        /**
+        @description Logout mutation
+        @method POST
+        @access public
+        */
         logout: builder.mutation({
             query: () => ({
                 url: `${USERS_URL}/logout`,
