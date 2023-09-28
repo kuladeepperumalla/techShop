5bb7fab25280779068182853e3351aa29bb0d131
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-09-28 12:26:25 -0400

    server js fixes

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-const orderRoutes =  require('./routes/orderRoutes.js')
-// import orderRoutes from './routes/orderRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
